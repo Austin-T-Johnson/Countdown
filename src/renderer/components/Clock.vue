64ad22e67b1c6f113b3057f60e1d25215654c374
Austin Johnson <austintjohnson92@gmail.com>
2025-11-12 21:37:58 -0800

    update clock bug on output monitor

--- src/renderer/components/Clock.vue
+++ src/renderer/components/Clock.vue
@@ -1,55 +1,72 @@
 <script setup lang="ts">
-import {ClockIcon} from '@heroicons/vue/24/solid';
+import { ClockIcon } from "@heroicons/vue/24/solid";
 import dayjs from "dayjs";
-import {onMounted, ref} from "vue";
+import { onMounted, onUnmounted, ref, watch } from "vue";
 
 export interface Props {
-  isBig: boolean
-  clockColor: string
-  textColor: string
-  secondsOnClock: boolean
-  use12HourClock: boolean
+  isBig: boolean;
+  clockColor: string;
+  textColor: string;
+  secondsOnClock: boolean;
+  use12HourClock: boolean;
 }
 
 const props = defineProps<Props>();
 
-let currentTimeTimerId: NodeJS.Timer = null;
-let currentTime = ref(dayjs().format('HH:mm'));
+let currentTimeTimerId: ReturnType<typeof setInterval> | null = null;
+let currentTime = ref(dayjs().format("HH:mm"));
 function updateTime() {
   if (props.secondsOnClock && !props.use12HourClock) {
-    currentTime.value = dayjs().format('HH:mm:ss');
+    currentTime.value = dayjs().format("HH:mm:ss");
   } else if (props.secondsOnClock && props.use12HourClock) {
-    currentTime.value = dayjs().format('hh:mm:ss a')
+    currentTime.value = dayjs().format("hh:mm:ss a");
   } else if (!props.secondsOnClock && props.use12HourClock) {
-    currentTime.value = dayjs().format('hh:mm a')
+    currentTime.value = dayjs().format("hh:mm a");
   } else {
-    currentTime.value = dayjs().format('HH:mm');
+    currentTime.value = dayjs().format("HH:mm");
   }
 }
 
+// Watch for prop changes and update time format immediately
+watch(
+  [() => props.use12HourClock, () => props.secondsOnClock],
+  () => {
+    updateTime();
+  },
+  { immediate: true }
+);
+
 onMounted(() => {
   if (currentTimeTimerId === null) {
-    currentTimeTimerId = setInterval(updateTime, 1000)
+    currentTimeTimerId = setInterval(updateTime, 1000);
   }
-})
+});
+
+onUnmounted(() => {
+  if (currentTimeTimerId !== null) {
+    clearInterval(currentTimeTimerId);
+    currentTimeTimerId = null;
+  }
+});
 </script>
 
 <template>
   <div
     class="text-center text-clock font-digital-clock"
     :class="{
-        'text-clock': !isBig,
-        'text-clock-on-reset': isBig
-      }"
+      'text-clock': !isBig,
+      'text-clock-on-reset': isBig,
+    }"
   >
     <clock-icon
       class="inline-block"
       :class="{
-          'clock-icon': !isBig,
-          'clock-icon-on-reset': isBig
-        }"
-      :style="{color: clockColor}" />
-    <span class="ml-5" :style="{color: textColor}">{{ currentTime }}</span>
+        'clock-icon': !isBig,
+        'clock-icon-on-reset': isBig,
+      }"
+      :style="{ color: clockColor }"
+    />
+    <span class="ml-5" :style="{ color: textColor }">{{ currentTime }}</span>
   </div>
 </template>
 
